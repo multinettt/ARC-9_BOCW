74c0f184b946cf6c2f401f0efc30f060c4d0f0a9
multinett <72457258+multinettt@users.noreply.github.com>
2022-12-09 16:56:54 +0300

    AN/PVS-4 & PN58 Scopes

---
+++ lua/arc9/common/attachments/bocw_optic_nochsovathermal.lua
@@ -0,0 +1,69 @@
+ATT.PrintName = "Noch Sova Thermal 2.3x"
+ATT.CompactName = "PN58"
+ATT.Icon = Material("entities/bocw_atts/optics/anpvs4.png", "mips smooth")
+ATT.Description = [[Soviet thermal scope with 2.3x magnification improves visibility by highlighting enemy heat signatures.]]
+ATT.SortOrder = 2.3
+
+ATT.Model = "models/weapons/arc9/atts/bocw_scope_nochsovathermal.mdl"
+
+ATT.Category = "optic_ak47"
+ATT.Folder = "BOCW SCOPE" -- test
+
+ATT.Sights = {
+    {
+        Pos = Vector(0, 6.05, -1.37),
+        Ang = Angle(0, 0, 0),
+        Magnification = 1,
+        ViewModelFOV = 60
+    },
+    -- {
+    --     Pos = Vector(0, 7.5, -3.237175),
+    --     Ang = Angle(0, 0, 0),
+    --     Magnification = 1.15,
+    --     Disassociate = true
+    -- }
+}
+
+ATT.RTScopeAdjustable = false
+
+ATT.RTScope = true
+ATT.RTScopeSubmatIndex = 1
+ATT.RTScopeFOV = 7.7
+ATT.RTScopeReticle = Material("arc9/reticle_nochsova.png", "mips smooth")
+ATT.RTScopeReticleScale = 1.2
+ATT.RTScopeColorable = true
+ATT.RTScopeShadowIntensity = 10
+
+ATT.RTScopeFLIR = true
+ATT.RTScopeFLIRSolid = false -- Solid color FLIR instead of like a shaded look
+ATT.RTScopeFLIRRange = 30000
+ATT.RTScopeFLIRMonochrome = false
+ATT.RTScopeFLIRBlend = 0.1
+ATT.RTScopeFLIRNoPP = true
+ATT.RTScopeFLIRCCHot = { -- Color correction drawn only on FLIR targets
+    ["$pp_colour_addr"] = 200,
+    ["$pp_colour_addg"] = 0,
+    ["$pp_colour_addb"] = 0,
+    ["$pp_colour_brightness"] = 0,
+    ["$pp_colour_contrast"] = 1,
+    ["$pp_colour_colour"] = 1,
+    ["$pp_colour_mulr"] = 0,
+    ["$pp_colour_mulg"] = 0,
+    ["$pp_colour_mulb"] = 0
+}
+ATT.RTScopeFLIRCCCold = {
+    ["$pp_colour_addr"] = 0,
+    ["$pp_colour_addg"] = 0,
+    ["$pp_colour_addb"] = 2,
+    ["$pp_colour_brightness"] = 0,
+    ["$pp_colour_contrast"] = 0.15,
+    ["$pp_colour_colour"] = 1,
+    ["$pp_colour_mulr"] = 0,
+    ["$pp_colour_mulg"] = 0,
+    ["$pp_colour_mulb"] = 0
+}
+
+ATT.ScopeScreenRatio = 0.7
+
+ATT.ModelOffset = Vector(0, 0, 0.087)
+ATT.Scale = 1
