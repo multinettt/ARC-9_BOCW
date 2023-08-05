f99db6ac9004c8c2f4ed84f4baaf8b2964841c6a
multinett <72457258+multinettt@users.noreply.github.com>
2023-08-05 17:49:40 +0300

    New Weapon: FFAR 1

--- lua/arc9/common/attachments_bulk/bocw_assaultrifles_muzzle.lua
+++ lua/arc9/common/attachments_bulk/bocw_assaultrifles_muzzle.lua
@@ -47,3 +47,27 @@ ATT.MuzzleDevice = true
 ATT.MenuCategory = "ARC9 - BOCW Attachments"
 
 ARC9.LoadAttachment(ATT, "bocw_qbz83_muzzle_base")
+
+ATT = {}
+
+ATT.PrintName = "FFAR1 Muzzlebrake"
+ATT.CompactName = "FFR1 MUZZ"
+ATT.Icon = Material("entities/acwatt_bocw.png", "mips smooth")
+ATT.Description = [[ ]]
+
+ATT.SortOrder = -10
+
+ATT.Model = "models/weapons/arc9/atts/bocw_ffar1_muzzle.mdl"
+
+ATT.Category = {"bocw_ffar1_muzzle"}
+
+ATT.ModelOffset = Vector(0, 0, 0)
+ATT.ModelAngleOffset = Angle(0, 0, 0)
+
+ATT.MuzzleParticleOverride = "muzzleflash_6"
+ATT.MuzzleParticleOverride_Priority = 10
+ATT.MuzzleDevice = true
+
+ATT.MenuCategory = "ARC9 - BOCW Attachments"
+
+ARC9.LoadAttachment(ATT, "bocw_ffar1_muzzle_base")
--- lua/arc9/shared/sh_arc9_bocw_assaultrifles_sounds.lua
+++ lua/arc9/shared/sh_arc9_bocw_assaultrifles_sounds.lua
@@ -680,3 +680,216 @@ sound.Add({
     volume = 1.0,
     sound = "weapons/arc9/bocw_qbz83/qbz83_fire_silenced.wav"
 })
+
+--========== FFAR1 ============================================================
+-- MAGAZINE
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_reload_magout",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_reload_magout.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_reload_magin",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_reload_magin.wav"
+})
+
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_reload_empty_magout",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_reload_empty_magout.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_reload_empty_magin",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_reload_empty_magin.wav"
+})
+
+-- BOLT OPERATION
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_boltback",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_boltback.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_boltrelease",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_boltrelease.wav"
+})
+
+-- RELOAD FOLEY
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_reload_start",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_reload_start.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_ready_end",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_ready_end.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_reload_end",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_reload_end.wav"
+})
+
+-- INSPECT FOLEY
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_inspect_part1",
+    channel = CHAN_ITEM,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_inspect_part1.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_inspect_part2",
+    channel = CHAN_ITEM,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_inspect_part2.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_inspect_music",
+    channel = CHAN_ITEM,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_inspect_music.wav"
+})
+
+-- PULLOUT PUTAWAY
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_draw",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_draw.wav"
+})
+
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_holster",
+    channel = CHAN_AUTO,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_holster.wav"
+})
+
+-----------------------------------------------
+-- FIRING SOUNDS
+
+sound.Add({ -- BLACK OPS COLD WAR
+    name = "ARC9_BOCW.FFAR1_fire",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    level = 100,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_fire.wav"
+})
+--[[
+sound.Add({ -- WARZONE
+    name = "ARC9_BOCW.FFAR1_fire_wz",
+    channel = CHAN_STATIC,
+    volume = 0.7,
+    level = 100,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_fire_warzone.wav"
+})
+
+sound.Add({ -- BLACK OPS
+    name = "ARC9_BOCW.FFAR1_fire_bo",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    level = 100,
+    sound = { "weapons/arc9/bocw_ffar1/ffar1_fire_bo_1.wav", "weapons/arc9/bocw_ffar1/ffar1_fire_bo_2.wav","weapons/arc9/bocw_ffar1/ffar1_fire_bo_3.wav", "weapons/arc9/bocw_ffar1/ffar1_fire_bo_4.wav", "weapons/arc9/bocw_ffar1/ffar1_fire_bo_5.wav" }
+})
+]]
+sound.Add({
+    name = "ARC9_BOCW.FFAR1_fire_silenced",
+    channel = CHAN_STATIC,
+    volume = 1.0,
+    sound = "weapons/arc9/bocw_ffar1/ffar1_fire_silenced.wav"
+})
+
+--[[---------------------------------------------------------------------------------
+
+In the year 2024 I will be taking part in the YKS(Higher-Education Institutions Exam).
+
+This type of exam requires at least 2 years of preparation. It does not mess around.
+
+Futures of bright minded students are decided by this cruel exam system.
+
+You can learn more by translating this page to english:
+https://tr.wikipedia.org/wiki/Yükseköğretim_Kurumları_Sınavı
+
+To cut the crap, what this means is I will go on a very long hiatus for over 10 months.
+
+In order to mentally help myself, I have placed many easter egg type of math questions
+in many of my weapons for years now, featuring the hardest math topics that I am
+responsible for.
+
+So yeah thats the whole reason why I put these here, for the 2 people that knows this
+in the first place.
+
+I truly believe math enlightens ones brain and I would like you to at least try to solve
+what I place here.
+
+----------------------------------------------------------------------
+
+(aₙ) real numbers sequence has this equality for every positive n integer:
+
+    aₙ + (-1)ⁿ⋅aₙ₊₁ = 2ⁿ
+
+If we consider a₁ = 0 , what is a₃ + a₄ + a₅ + a₆ equal to?
+
+A) 6    B) 8    C) 12   D) 16   E) 20
+
+
+The answers will be revealed in around 2 weeks, and when I remember to.
+
+
+
+Here's another one!
+
+
+(bₙ), for which its first two elements are b₁ = 4/3 and b₂ = 2 , geometric sequence and
+(aₙ), is an arithmatic sequence in which its common difference is equal to bₙ's common
+factor; are given.
+
+b₇ = a₁₁ , so what is a₁?
+
+A) 1/4  B) 1/8  C) 3/8  D) 3/16  E) 5/16
+
+
+
+
+
+
+Check this one out too!
+
+
+x is a positive real number.
+
+log₄(x+5) + log₄(x+4) - log₄(x+3) = log₂3
+
+So what is x's value?
+
+A) √6   B) √7   C) 2√2  D) 2√5  E) 3√2
+
+
+
+]]
